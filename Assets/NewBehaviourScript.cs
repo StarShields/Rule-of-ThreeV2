c467263e5ba5d932c1dca8c6bded5615a0e28815
AJ <jumperthejumper@gmail.com>
2021-03-29 16:08:15 -0500

    more stuff

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public PlayerCollision collision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        int numCollisions = 0;
+
+        if(collisionInfo.collider.tag == "player")
+        {
+            if(numCollisions % 3 == 0)
+            {
+         //       lives++;
+            }
+        }
+    }
+
+}
---
+++ Assets/movment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour
+{
+    public float speed = 15f;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+
+
+    }
+}
