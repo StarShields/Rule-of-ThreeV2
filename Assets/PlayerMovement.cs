a7299f216b7dc4ba062717ac5b801dbc70d3a1ec
AJ <jumperthejumper@gmail.com>
2021-03-31 03:57:28 -0500

    Start Menu Integration

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float sidwaysForce = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidwaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidwaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
